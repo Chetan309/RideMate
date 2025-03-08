d09edb555faa5dc3eb6a280cbb5d08138f9fb457
Chetan309 <Chetan309044@gmail.com>
2025-03-08 16:20:51 +0530

    Updated

--- app/page.js
+++ app/page.js
@@ -1,19 +1,16 @@
-import { SignIn, SignUp } from "@clerk/nextjs";
-import Image from "next/image";
 
-export default function Home() {
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+export default function SignInPage() {
   return (
+    <>
     <div className="relative">
-      <Image 
-        src="/RideMateBanner.jpg" 
-        width={900} 
-        height={1000} 
-        alt="RideMate Banner"
+      <Image src="/RideMateBanner.jpg" width={1000} height={1000} alt="RideMate Banner"
         className="object-contain w-full h-full"/>
-        <div className="absolute top-25 right-0  ">
-          <SignIn/>
-        </div>
+      <div className="absolute top-25 right-0">
+        <SignIn />
+      </div>
     </div>
-  )
+    </>
+  );
 }
- 
